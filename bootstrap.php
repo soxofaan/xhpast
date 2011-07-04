854344964549909d9927bcc5d72bcca11133e89d
Sean Coates <sean@seancoates.com>
2011-07-04 17:38:34 -0400

    bootstrap helper (loader)

---
+++ bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+require __DIR__ . '/XHPASTSyntaxErrorException.php';
+require __DIR__ . '/XHPASTNodeList.php';
+require __DIR__ . '/XHPASTNode.php';
+require __DIR__ . '/XHPASTToken.php';
+require __DIR__ . '/XHPASTTree.php';
+require __DIR__ . '/utils.php';
+require __DIR__ . '/xhpast_parse.php';
+
+
